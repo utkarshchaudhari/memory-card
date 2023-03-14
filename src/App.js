6bc8018701f91c1ff84413d15d24c299d1f7eead
utkarshchaudhari <utkarshchaudhari1996@gmail.com>
2023-03-14 16:14:24 -0400

    created footer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import './style.css';
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
